fb56f520b29fe10f996e23f14a5bd5fa031537ec
BArthur1972 <82739341+BArthur1972@users.noreply.github.com>
2022-04-26 14:06:30 -0500

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,2 @@
+javac --module-path /Users/Bethel/Desktop/javafx-sdk-18/lib --add-modules javafx.controls,javafx.fxml CalculatorApp.java
+java --module-path /Users/Bethel/Desktop/javafx-sdk-18/lib --add-modules javafx.controls,javafx.fxml CalculatorApp
