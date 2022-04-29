7db67c7e2a9802d84402f71ec1e13a872078617f
Emmanuelikeh <88993873+Emmanuelikeh@users.noreply.github.com>
2022-04-29 04:50:57 +0100

    update commit

--- Utilities.java
+++ Utilities.java
@@ -1,12 +1,24 @@
+
 // basic libraries necessary in the implementation 
+
 // are imported and used, including javaFx
 
+// this file sevs as the drive force of the app
+
+// app function here affect the way the app behaves
+
 import javafx.application.Application;
+
 import javafx.stage.*;
+
 import javafx.scene.*;
+
 import javafx.scene.image.*;
+
 import javafx.scene.control.*;
+
 import javafx.scene.layout.*;
+
 import javafx.scene.control.*;
 import javafx.event.*;
 import javafx.geometry.*;
@@ -20,7 +32,9 @@ import java.math.*;
 import java.text.DecimalFormat;
 
 // main class that serves as 
+
 // control house, to drive
+
 // the calculators functionality
 
 public class Utilities 
@@ -31,27 +45,41 @@ public class Utilities
 	int operator = 0;
 
 	double a = 0;
+
 	double b = 0;
 
 	double result = 0;
 
 //For mortgage calculation functionality 
+
 // P reprsents the principal,
+
 // Principal is the money that you originally agreed to pay back. 
+
 // Starts the mortgage calculation process
+
 	double p = 0;
+
+
 	
 // R reprsents the rate
+
 // The interest rate is the amount a lender charges a borrower 
+
 //and is a percentage of the principal—the amount loaned. 
+
 //The interest rate on a loan is typically noted on an annual basis 
+
 //known as the annual percentage rate (APR). 
 	double r = 0;
 	
 	
 // T reprsents the time
+
 //time take for the mortage loan to be paid
+
 // has a massive contribution to the toatl outcome 
+
 // of the loan with intrest
 	int t = 0;
 	
@@ -62,7 +90,9 @@ public class Utilities
 	
 	
 // function for the clear button
+
 // replaces text on the text area with an empty string
+
 // indirectly clearing everything on the text area
 	
 	public void clear(TextArea textArea) 
@@ -96,8 +126,13 @@ public class Utilities
 			//for loop to go over items in the extracted string in the text view
 			
 			textArea.setText(textArea.getText()+s.charAt(i));
+
+
 		}
 	}
+
+
+
 	
 	
 // evaluates the sin operation
@@ -122,8 +157,12 @@ public class Utilities
 		// presents the results in the text area
 		
 		textArea.setText("sin"+"("+a+")"+ "\n"+ s_Result);
+
+
 	}
 	
+
+
 // evaluates the cos operation
 	
 	public void getCos (TextArea textArea) 
@@ -144,7 +183,13 @@ public class Utilities
 		
 		// returns the result to the user on the textArea
 		textArea.setText("cos"+"("+a+")"+ "\n"+ s_Result);
+
+
+
 	}
+
+
+
 	
 	// evaluates the tan operation
 
@@ -169,7 +214,11 @@ public class Utilities
 		// returns the result to the user on the textArea
 		
 		textArea.setText("tan"+"("+a+")"+ "\n"+ s_Result);
+
+
 	}
+
+
 	
 // evaluates the log operation
 
@@ -195,7 +244,11 @@ public class Utilities
 		// returns the result to the user on the textArea
 		
 		textArea.setText("log"+"("+a+")"+ "\n"+ s_Result);
+
+
 	}
+
+
 	
 	// evaluates the sinInverse operation
 
@@ -230,9 +283,13 @@ public class Utilities
 				
 				// returns the result to the user on the textArea
 				textArea.setText("sin-1("+a+")"+"\n"+s_Result);
+
+
 			}
 
 	}
+
+
 	
 	// evaluates the cosInverse operation
 
@@ -264,239 +321,760 @@ public class Utilities
 				
 				// returns the result to the user on the textArea
 				textArea.setText(s_Result);
+
+
 			}
 
 	}
 
+
+
+// evaluates the TanInverse operation
 
 	public void getTanInverse(TextArea textArea)
+
+	// doesn not return anything
 	{
 		String s_Result = "";
+
+		// current context in textarea
+
+
 		a = Double.parseDouble(textArea.getText());
+
+		// converts double to radians for  operation
+
+		//to be perfomed on
+
+
 		result = Math.atan(a);
+
+		//performs the coseInverse operation
+
 		result = Math.toDegrees(result);
+
+		//converts result to degerees
+
+		//converts the result to a string
+
 		s_Result = Double.toString(result);
+
+		// returns the result to the user on the textArea
+
 		textArea.setText("tan-1("+a+")"+"\n"+s_Result);
+
+
 	}
 
+
+
+// evaluates the TanInverse operation
 
 	public void getSquareRoot(TextArea textArea)
+
+	// doesn not return anything
+
 	{
+
 		String s_Result = "";
+
+		// current context in textarea
+
+
 		a = Double.parseDouble(textArea.getText());
+
+		//performs the square root operation
+
 		result = Math.sqrt(a);
+
+		//performs the coseInverse operation
+
+		//converts result to degerees
+
+		//converts the result to a string
+
+
 		String f_result = Double.toString(result);
+		//converts the result to a string
+
+
+		// returns the result to the user on the textArea
 		textArea.setText(f_result);
+
+
 	}
 
+
+
+// evaluates the square operation
 
 	public void getSquare(TextArea textArea)
+
+	// doesn not return anything
+
 	{
 		String s_Result = "";
+
+		// current context in textarea
+
 		a = Double.parseDouble(textArea.getText());
+		//performs the square operation
+
+
 		result = a * a;
+
+		//performs the square operation
+
+		//converts result to degerees
+
+		//converts the result to a string
+
+
 		String f_result = Double.toString(result);
+		//converts the result to a string
+
+		// returns the result to the user on the textArea
+
 		textArea.setText(f_result);
+
+
 	}
 
+
+
+// evaluates the pie button 
 
 	public void getPie(TextArea textArea)
+
+	// doesn not return anything
+
+
 	{
 		String s_Result = "";
+
+		// current context in textarea
+
+
 		result = Math.PI;
+
+		//get pi value
+
+		//obtains it from math funtion
+
+		//converts the result to a string
+
 		s_Result = Double.toString(result);
+		//converts the result to a string
+
+		// returns the result to the user on the textArea
+
 		textArea.setText(s_Result);	
+
+
 	}
+
+
+//evaluates  the exponential button
 
 	public void getExponential(TextArea textArea)
+
+	// doesn not return anything
+
 	{
+
 		String s_Result = "";
+
+		// current context in textarea
+
+
 		a = Double.parseDouble(textArea.getText());
+
+
 		result = Math.exp(a);
+
+		//get exponential value
+
+		//obtains it from math funtion
+
+		//converts the result to a string
+
+
 		String f_result = Double.toString(result);
+
+		//converts the result to a string
+
+		// returns the result to the user on the textArea
+
+
 		textArea.setText(f_result);
+
+
 	}
+
+
+//evaluates the percentage button
+
 
 	public void getPercentage(TextArea textArea)
+
+	// doesn not return anything
+
 	{
+
+
 		String s_Result = "";
+
+		// current context in textarea
+
+
 		a = Double.parseDouble(textArea.getText());
+
+		// converts string to be operated on 
+
+
 		result = a/100;
+
+		//ezpresses value in function
+
+		//obtains it from math funtion
+
+		//converts the result to a string
+
 		String f_result = Double.toString(result);
+
+		//converts the result to a string
+
+		// returns the result to the user on the textArea
+
+
 		textArea.setText(f_result);
+
+
 	}
+
+
+
+	
+//evaluates the addition function
 
 	public void addPlus(TextArea textArea)
+	// does not return anuthing
 	{
+		//converts content in text area to be operated on
 		a = Double.parseDouble(textArea.getText());
+		//changes it to a double for operatiom
+		//represents switch value
 		operator = 1;
+		//sets result in text arae
 		textArea.setText("");	
+
+
 	}
+
+
+// stores previous answer
+
+	//vital in further addition process and other
+
+	// vital in multiple arithemtic funtion continuatlity 
 
 	public void storeAnswer(TextArea textArea)
+
+	//does not return anything
+
+	//a void funtion
+
 	{	
-		textArea.setText(Double.toString(result));	
+	//converts context in arae to be operated
+
+		//on by the funtion
+
+		textArea.setText(Double.toString(result));
+
+		//changes it to a double for operation
+
+		/// sets result in text area
+
+
 	}
+
+
+
+	//adds minus funtion
 
 	public void addMinus(TextArea textArea)
+
+
+	//vital in further addition process and other
+
+	// vital in multiple arithemtic funtion continuatlity 
+
 	{
+
 		if (textArea.getText().isEmpty())
+
+			//adds the minus sign
+
+
+			//if it is empty
+
+
 			{
+
 				textArea.appendText("-");
+
 			}
+
+
 			else
+
 			{
+
+				//parse the content in text area
+
+				// to a double
+
 				a = Double.parseDouble(textArea.getText());
+
 				operator = 2;
+
+				//represnts switch operation
+
+				//sects text after perofoming operation
+
 				textArea.setText("");
+
+
 			}	
+
+
 	}
 
+
+//adds minus funtion
 
 	public void addDivide(TextArea textArea)
+
+		//vital in further addition process and other
+
+	// vital in multiple arithemtic funtion continuatlity 
+
+
+
 	{
+
+		//adds the divide sign
+
+
+			//if it is empty
+
+		//parse the content in text area
+
+				// to a double
+
+
 		a = Double.parseDouble(textArea.getText());
+
+		//represnts switch operation
+
+				//sects text after perofoming operation
+
+
+
 		operator = 4;
+
+
+		//sects text after performing operation
+
+
+		// Setting text Area empty
 		textArea.setText("");	
+
+
 	}
+
+
+
+	// This method updates the operator to multiplication 
 
 	public void addMultiply(TextArea textArea)
 	{
+		
+		// Update a
 		a = Double.parseDouble(textArea.getText());
+
+		// Update operator to multiplication
 		operator = 3;
+
+		// Update the operator to empty
+
 		textArea.setText("");
+
+		// Print operator to the terminal
 		System.out.println("operator is " + operator);
 	}
 
+
+
+
+	// Equals method to obtain results
+
+
 	public void equals(TextArea textArea) {
 
+		// Updates b
+
 		b = Double.parseDouble(textArea.getText());
+
+		// Prints a and b
 		System.out.println("Equal button pressed " + "a = " + a + ", b = " + b);
 			
+
 		// Setting up BigDecimals to handle high precision numbers
 		String first = Double.toString(a);
 		String second = Double.toString(b);
+
+
+		// Creating Big decimal objects
+		
+
+		// bigNum 2
 		BigDecimal biggerNum1 = new BigDecimal(first);
+
+		// bigNum 2
 		BigDecimal biggerNum2 = new BigDecimal(second);
 
+		// Handles which operation gets called
 		handleOperation(textArea, b, biggerNum1, biggerNum2);
+
+
 	}
 
-	public void handleOperation(TextArea textArea, double b, BigDecimal reallyBig, BigDecimal reallyBig1) {
+	// Does the calculations
 
+	public void handleOperation(
+
+		// Text area that displays results		
+		TextArea textArea,
+
+		// Second value
+		double b,
+
+		// first number
+		BigDecimal reallyBig,
+
+		// second number
+		BigDecimal reallyBig1
+		) 
+	{
+
+
+		// T_result for total
 		String T_result = "";
+
+
 		BigInteger intValue;
 		BigDecimal decValue;
 
 		switch(operator)
+
 		{
+		// First case for addition
+			
 		case 1:
+
+			// adds the two numbers
 			BigDecimal result1 = reallyBig.add(reallyBig1);
+
+
+
 			intValue = result1.toBigInteger();
+
+
+			// Converts tresult to string
+
 			T_result = intValue.toString();
+
+			// Check if result is less than 9
+
 			if (T_result.length() > 9)
+
 			{
+				// Convert result to string
+
 				T_result = result1.toString();
+
+				// Set text area to equation
+
 				textArea.setText(reallyBig +" + "+reallyBig1 +" = "+ T_result);
+
+				// Convert result to double
+
 				result = Double.parseDouble(T_result);
+
 			}
+
 			else
+
 			{
+
+				// Convert result to string
 				T_result = result1.toString();
+
+				// Set Text Area to equation 
+
 				textArea.setText(reallyBig +" + "+reallyBig1 +" = "+ T_result);
+
+				// Convert result to double
 				result = Double.parseDouble(T_result);
+
+
 			}
+
 			break;
 		
+		// Case  2 for subtraction
+
 		case 2:
+
+			// Subtracting numbers
+
 			BigDecimal result2 = reallyBig.subtract(reallyBig1);
+
+			// Converting result to Big Integer
 			intValue = result2.toBigInteger();
+
+			// Converting result to String
 			T_result = intValue.toString();
+
+
+			// Check if length result is less than 9
 			if (T_result.length() > 9)
+
 			{
+				// Converts result to String
 				T_result = result2.toString();
+
+				// Sets textArea to equation
 				textArea.setText(reallyBig +" - "+reallyBig1 +" = "+ T_result);
-				result = Double.parseDouble(T_result);
-			}
-			else
-			{
-				T_result = result2.toString();
-				textArea.setText(reallyBig +" - "+reallyBig1 +" = "+ T_result);
+
+				// Converts results to double
 				result = Double.parseDouble(T_result);
 
 			}
-			break;
-	
-		case 3:
-			BigDecimal result3 = reallyBig.multiply(reallyBig1);
-			intValue = result3.toBigInteger();
-			T_result = intValue.toString();
-			if (T_result.length() > 9)
-			{
-				T_result = result3.toString();
-				textArea.setText(reallyBig +" * "+reallyBig1 +" = "+ T_result);
-				result = Double.parseDouble(T_result);
-			}
+
 			else
+
 			{
+				// Converts result to String
+
+				T_result = result2.toString();
+
+				// Sets text area to Equation
+
+				textArea.setText(reallyBig +" - "+reallyBig1 +" = "+ T_result);
+
+				// Converts result to double
+
+				result = Double.parseDouble(T_result);
+
+			}
+			
+
+			// Breaks the program
+			break;
+		
+
+		// Case 4 for multiplication
+		case 3:
+
+			// Multiplying the two numbers
+
+			BigDecimal result3 = reallyBig.multiply(reallyBig1);
+
+
+			// Adding intValue
+
+			intValue = result3.toBigInteger();
+
+			// Convert the result to string
+
+			T_result = intValue.toString();
+
+			// Checks if length of result is greater than 9
+
+
+			if (T_result.length() > 9)
+
+			{
+
+				// Converts result to string
 				T_result = result3.toString();
+
+				// Sets text area to equation
 				textArea.setText(reallyBig +" * "+reallyBig1 +" = "+ T_result);
+
+				// Converts result to double
 				result = Double.parseDouble(T_result);
 			}
+
+
+			else
+
+			{
+
+				// Converts result3 to string
+
+				T_result = result3.toString();
+
+				// Sets text area to equation
+
+				textArea.setText(reallyBig +" * "+reallyBig1 +" = "+ T_result);
+
+				// updates result
+
+				result = Double.parseDouble(T_result);
+
+			}
+
 			break;
 			
+		// Case for for division
+
 		case 4:
+
+			// Divides a and b
 			result += a / b;
 
+
+			// Sets f_result to result
+
 			Double f_result = result;
+
+			// Converts f_result to string
+
 			String s = Double.toString(f_result);
+
+			// Checks of string s is enfinity
+
 			if (s.equals("Infinity"))
+
 			{
+				// Assigns result to error
+
 				T_result = "Error";
+
+				// Sets textArea to result
+
 				textArea.setText(T_result);
+
+				// Converts result to Double
 				result = Double.parseDouble(T_result);
+
 			}
-			else	
+
+			else
+
+				// Converts result to string
 				T_result = Double.toString(result);
+
+				// Sets text area to equation
 				textArea.setText(reallyBig +" / "+reallyBig1 +" = "+ T_result);
+
+				// Converts result to double
 				result = Double.parseDouble(T_result);
-			
+		
+
+		// Sets default case
+
 		default:
+
+			// Sets result to 0
 			result = 0;
+
 		}
+
 	}
 
 
-	public void addPrincipal(TextArea textArea) {
+	// Extracts the principal from the text area
+	public void addPrincipal(TextArea textArea) 
+
+	{
+		// Updates p
 		p = Double.parseDouble(textArea.getText());
+
+		// Sets text area to value of p
 		textArea.setText("Principal:"+p);
+
 	}
 
-	public void addRate(TextArea textArea) {
+	// Extracts the rate from the textArea
+	public void addRate(TextArea textArea) 
+
+	{
+		// Updates r
 		r = Double.parseDouble(textArea.getText());
+
+		// Sets text area to value of p and r		
 		textArea.setText("Principal:"+p+", Rate:"+r+"%");
+
 	}
 
-	public void addTime(TextArea textArea) {
+	// Extracts the time from the text Area 
+	public void addTime(TextArea textArea) 
+
+	{
+
+		// Updates t
 		t = Integer.parseInt(textArea.getText());
+
+		// Sets text area to value of the principal, rate and payments
 		textArea.setText("Principal:"+p+", Rate:"+r+"%"+", Payments:"+t);
+
 	}
 
-	public void getMortgage(TextArea textArea) {
+	// Extract the mortgage from the text area
+
+	public void getMortgage(TextArea textArea) 
+
+	{
+		// Sets text area to null
 		textArea.setText("");
 
+		// divide r by 100 and then by 12
 		r = (r/100)/12;
+
+		// format it in decimal format
 		String format_r = df.format(r);
+
+		// Find time in months
 		t = t *12;
 
+		// Calculates the mortgage
+
 		Double pay = (p*r)/(1-Math.pow(1+r,-t));
+
+		// Formats the result in decimal form
+
 		String format_pay = df.format(pay);
+
+		// Sets text area to null
 
 		textArea.setText(" ");
 
-		textArea.setText("Principal:" + p + "\nRate:" + format_r
-		+ "\nNo_payment:" + t + "\n" +"Mortgage:" + format_pay);
+
+		// Displays mortgage on text area
+		textArea.setText(
+			"Principal:" + p + 
+			"\nRate:" + format_r +
+			"\nNo_payment:" + t +
+			"\n" +"Mortgage:" + format_pay
+		);
 	}
 
+
+	// TESTING CODE /
+	// End class
 
 }
